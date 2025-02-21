0784f64ffc8b48e5ba7d98e7b09732f6bfc4051c
Clarissa Mobley <clarissamobley@me.com>
2025-02-21 17:34:16 -0500

    initial home page setup

---
+++ src/frontend/pages/index.js
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+    return (
+        <div className="min-h-screen bg-white">
+            <header className="bg-black text-white p-4 text-center">
+                <h1 className="text-3xl font-bold">WanderBook</h1>
+                <p>Your next adventure made easy.</p>
+            </header>
+
+        </div>
+    )
+}
